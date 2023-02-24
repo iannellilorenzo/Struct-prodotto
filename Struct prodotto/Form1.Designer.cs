d010dfd8402bc6a05a261c5a3684777f3116d4f1
Iannelli Lorenzo <iannelli.lorenzo.studente@itispaleocapa.it>
2023-02-24 12:54:15 +0100

    cambiamenti

--- Struct prodotto/Form1.Designer.cs
+++ Struct prodotto/Form1.Designer.cs
@@ -28,21 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Name = new System.Windows.Forms.TextBox();
+            this.Price = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listView = new System.Windows.Forms.ListView();
+            this.salva = new System.Windows.Forms.Button();
+            this.Cancellazione = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // Name
+            // 
+            this.Name.Location = new System.Drawing.Point(12, 28);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(100, 22);
+            this.Name.TabIndex = 0;
+            // 
+            // Price
+            // 
+            this.Price.Location = new System.Drawing.Point(156, 28);
+            this.Price.Name = "Price";
+            this.Price.Size = new System.Drawing.Size(100, 22);
+            this.Price.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nome";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(153, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Prezzo";
+            // 
+            // listView
+            // 
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(416, 12);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(372, 426);
+            this.listView.TabIndex = 4;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.List;
+            this.listView.SelectedIndexChanged += new System.EventHandler(this.listView_SelectedIndexChanged);
+            // 
+            // salva
+            // 
+            this.salva.Location = new System.Drawing.Point(15, 82);
+            this.salva.Name = "salva";
+            this.salva.Size = new System.Drawing.Size(165, 31);
+            this.salva.TabIndex = 5;
+            this.salva.Text = "Aggiungi al carrello";
+            this.salva.UseVisualStyleBackColor = true;
+            this.salva.Click += new System.EventHandler(this.salva_Click_1);
+            // 
+            // Cancellazione
+            // 
+            this.Cancellazione.Location = new System.Drawing.Point(15, 119);
+            this.Cancellazione.Name = "Cancellazione";
+            this.Cancellazione.Size = new System.Drawing.Size(165, 26);
+            this.Cancellazione.TabIndex = 6;
+            this.Cancellazione.Text = "Elimina dal carrello";
+            this.Cancellazione.UseVisualStyleBackColor = true;
+            this.Cancellazione.Click += new System.EventHandler(this.Cancellazione_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Cancellazione);
+            this.Controls.Add(this.salva);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Price);
+            this.Controls.Add(this.Name);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox Name;
+        private System.Windows.Forms.TextBox Price;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.Button salva;
+        private System.Windows.Forms.Button Cancellazione;
     }
 }
 
--- Struct prodotto/Form1.cs
+++ Struct prodotto/Form1.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Struct_prodotto
 {
@@ -15,11 +17,80 @@ namespace Struct_prodotto
         public Form1()
         {
             InitializeComponent();
+            p = new prodotto[100];
+            dim = 0;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public struct prodotto
+        {
+            public string nome;
+            public float prezzo;
+        }
+
+        public prodotto[] p; // p è il nome, prodotto è il datatype
+        public int dim;
+
+        public string prodString(prodotto p)
+        {
+            return "Nome: " + p.nome + " Prezzo: " + p.prezzo.ToString();
+        }
+
+        public void visualizza(prodotto[] pp)
+        {
+            listView.Items.Clear();
+            for (int i = 0; i < dim; i++)
+            {
+                listView.Items.Add(prodString(p[i]));
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void salva_Click_1(object sender, EventArgs e)
+        {
+            p[dim].nome = Name.Text;
+            p[dim].prezzo = float.Parse(Price.Text);
+            dim++;
+            visualizza(p);
+        }
+
+        private void Cancellazione_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        // funz no-UI
+        public bool RicercaSQL()
+        {
+            bool search = true;
+            for (int i = 0; i < dim; i++)
+            {
+                if (p[i].ToString() == Name.Text)
+                {
+                    search = true;
+                }
+                else
+                {
+                    search = false;
+                }
+            }
+
+            return search;
         }
     }
 }
